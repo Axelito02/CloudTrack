74a4ca7e19cb9cc81b53c898c2c4af5081d35468
Kazura-Collins <98463668+Kazura-Collins@users.noreply.github.com>
2024-04-22 17:31:39 -0500

    CSS las publicaciones
    Ya tiene un loader, cards separadas, botón de borrar bonito. Ahora sigo con la pantalla de subir.

--- src/components/project-card/ProjectCard.jsx
+++ src/components/project-card/ProjectCard.jsx
@@ -1,29 +1,41 @@
-import React from 'react'
-// import styles from './ProjectsCard.module.css'
+import React, { useState } from 'react';
+import styles from './ProjectCard.module.css'; // CSS Modules
 
-export function ProjectCard ({ project, projectImage, onDelete }) {
-  const background = {
-    backgroundColor: 'white',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center'
-  }
+export function ProjectCard({ project, projectImage, onDelete }) {
+  const [isLoading, setIsLoading] = useState(true);
 
-  const projectId = project.projectId
-  const imageName = project.title.replace(/\s+/g, '') + project.projectId
+  const projectId = project.projectId;
+  const imageName = project.title.replace(/\s+/g, '') + project.projectId;
 
   const handleDeleteClick = () => {
-    onDelete(projectId, imageName)
-  }
+    onDelete(projectId, imageName);
+  };
+
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  };
+
+  const handleImageError = () => {
+    console.error('Error al cargar la imagen');
+  };
 
   return (
-    <div
-      className='proyect-card'
-      style={background}
-    >
-      <img src={projectImage} alt={project.title} style={{ width: '40vw', height: 'auto' }} />
-      <h2>{project.title}</h2>
-      <p>{project.description}</p>
-      <button onClick={handleDeleteClick}>Borrar</button>
+    <div className={styles.projectCardContainer}>
+      <div className={styles.projectCard}>
+        {isLoading && <div className={styles.loader}></div>} {/* Muestra el loader mientras carga */}
+        <img
+          src={projectImage}
+          alt={project.title}
+          style={{ width: '40vw', height: 'auto', display: isLoading ? 'none' : 'block' }}
+          onLoad={handleImageLoad}
+          onError={handleImageError}
+        />
+        <h2>{project.title}</h2>
+        <p>{project.description}</p>
+        <button className={styles.button} onClick={handleDeleteClick}>
+          Borrar
+        </button>
+      </div>
     </div>
-  )
+  );
 }
