a5bd0e38d7d200b29b92fa3528de8c3bf2f4b413
Axelito02 <alexvenegas183gmail.com>
2024-05-28 20:34:37 -0500

    arreglando css de la pagina de perfil

--- src/components/info-user/InfoUserProfile.jsx
+++ src/components/info-user/InfoUserProfile.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
 import styles from './InfoUserProfile.module.css';
 
 export function InfoUser() {
   const [inputsEnabled, setInputsEnabled] = useState(false);
+  const [phone, setPhone] = useState('+573004488715');
 
   const toggleInputs = () => {
     setInputsEnabled(!inputsEnabled);
@@ -33,7 +36,14 @@ export function InfoUser() {
               <h6>Rol:</h6>
             </label>
             <div className={`${styles.inputsInfo} ${inputsEnabled ? styles.enabled : ''}`}>
-              <input type='text' id='role' placeholder='Comercial' disabled={!inputsEnabled} />
+              <select className={styles.inputForm} id="role" defaultValue='5' disabled={!inputsEnabled}>
+                <option value='0' disabled hidden>Selecciona tu rol</option>
+                <option value='1'>Tecnico superior</option>
+                <option value='2'>Redes internas</option>
+                <option value='3'>Redes externas</option>
+                <option value='4'>Conexiones</option>
+                <option value='5'>Comercial</option>
+              </select>
             </div>
           </div>
           <div className={styles.ocupation}>
@@ -41,7 +51,7 @@ export function InfoUser() {
               <h6>Ocupación:</h6>
             </label>
             <div className={`${styles.inputsInfo} ${inputsEnabled ? styles.enabled : ''}`}>
-              <input type='text' id='ocupation' placeholder='Ingeniera civil' disabled={!inputsEnabled} />
+              <input type='text' id='ocupation' defaultValue='Ingeniera civil' placeholder='Ocupación' disabled={!inputsEnabled} />
             </div>
           </div>
         </div>
@@ -51,7 +61,17 @@ export function InfoUser() {
               <h6>Celular:</h6>
             </label>
             <div className={`${styles.inputsInfo} ${inputsEnabled ? styles.enabled : ''}`}>
-              <input type='text' id='number' placeholder='+57 3004488715' disabled={!inputsEnabled} />
+              <PhoneInput
+                country={'co'}
+                value={phone}
+                onChange={setPhone}
+                disabled={!inputsEnabled}
+                inputProps={{
+                  id: 'number'
+                }}
+                containerClass={styles.phoneInputContainer}
+                inputClass={`${styles.phoneInput} ${!inputsEnabled ? styles.disabledInput : ''}`}
+              />
             </div>
           </div>
           <div className={styles.email}>
@@ -59,7 +79,7 @@ export function InfoUser() {
               <h6>Correo electronico:</h6>
             </label>
             <div className={`${styles.inputsInfo} ${inputsEnabled ? styles.enabled : ''}`}>
-              <input type='text' id='email' placeholder='Laura@gmail.com' disabled={!inputsEnabled} />
+              <input type='text' id='email' defaultValue='Laura@gmail.com' placeholder='Correo electronico' disabled={!inputsEnabled} />
             </div>
           </div>
         </div>
--- src/pages/profilePage/ProfilePage.jsx
+++ src/pages/profilePage/ProfilePage.jsx
@@ -1,33 +1,31 @@
-import React from 'react'
-import styles from './ProfilePage.module.css'
-import { ButtonBack, InfoUser} from '../../components'
+import React from 'react';
+import styles from './ProfilePage.module.css';
+import { ButtonBack, InfoUser } from '../../components';
 
-export function ProfilePage () {
+export function ProfilePage() {
   return (
     <section className={styles.Main}>
-      <div className={styles.head}>
-        <div>
-          <ButtonBack/>
-        </div>
-        <div>
-          <h1>Perfil</h1>
-        </div>
-      </div>
-      <div className={styles.contentBanner}>
-        <div className={styles.contentBanner}>
-          <img src="../../../../assets/Banner.png"/>
+      <div className={styles.header}>
+        <div className={styles.bannerContainer}>
+          <img src="../../../../assets/Banner.png" alt="Banner" className={styles.bannerImg} />
+          <div className={styles.overlay}>
+            <div className={styles.headerContent}>
+              <ButtonBack />
+              <h1>Perfil</h1>
+            </div>
+          </div>
         </div>
       </div>
-      <div className={styles.contentProfileImgUser}>
-        <div className={styles.contentImgUser}>
-          <img src="../../../../assets/ProfileEdit.png" alt='User'/>
+      <div className={styles.content}>
+        <div className={styles.contentProfileImgUser}>
+          <img src="../../../../assets/ProfileEdit.png" alt='User' className={styles.profileImg} />
         </div>
-      </div>
-      <div className={styles.contentComponentInfoUser}>
-        <div className={styles.componentInfoUser}>
-          <InfoUser />
+        <div className={styles.contentComponentInfoUser}>
+          <div className={styles.componentInfoUser}>
+            <InfoUser />
+          </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
